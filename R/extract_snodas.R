12ce76917af65017ce03a257fe76ad082dd840b0
Zachary Hoylman <zh192885e@skypilot.geol.umt.edu>
2021-09-10 15:37:19 -0600

    finished extraction

--- R/extract_snodas.R
+++ R/extract_snodas.R
@@ -8,7 +8,7 @@ library(stringr)
 library(doParallel)
 library(foreach)
 
-var = 'runoff'
+var = 'precipitation'
 
 data.dir = paste0('/home/zh192885e/data/snodas/processed/', var, '/')
 
@@ -18,11 +18,13 @@ time = list.files(data.dir) %>% str_extract(., "(\\d)+")
 sf = st_read('/home/zh192885e/gnss-bucket-model/data/SLBHUC8/SLBHUC8.shp') %>%
   st_union()
 
-cl = makeCluster(2)
+cl = makeCluster(40)
 registerDoParallel(cl)
 
-#extraction = foreach(i = 1:length(files), .packages = c('dplyr', 'velox', 'raster')) %dopar% {
-extraction = foreach(i = 1:100, .packages = c('dplyr', 'velox', 'raster')) %dopar% {
+tictoc::tic()
+
+extraction = foreach(i = 1:length(files), .packages = c('dplyr', 'velox', 'raster')) %dopar% {
+#extraction = foreach(i = 1:100, .packages = c('dplyr', 'velox', 'raster')) %dopar% {
   gc()
   temp_rast = raster(files[[i]]) %>% velox
   
@@ -37,4 +39,6 @@ stopCluster(cl)
 
 final = bind_rows(extraction)
 
-write.csv(final, paste0('/home/zh192885e/data/summary/', var, '_selway_summary.csv'))
+write.csv(final, paste0('/home/zh192885e/local_data/', var, '_selway_summary.csv'), row.names = F)
+
+tictoc::toc()
